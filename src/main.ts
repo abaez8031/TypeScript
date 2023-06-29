87a9fae20511f8580d9aca031f8994ff8dcac011
abaez8031 <abaez8031@gmail.com>
2023-06-29 13:55:40 -0400

    or

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 let myName: string = "Dave";
 let meaningOfLife: number;
 let isLoading: boolean;
-let album: any;
+let album: string | number;
 
 myName = "John";
 meaningOfLife = 42;
 isLoading = true;
 album = "Van Halen";
 album = 1984;
-album = true;
 
 
 const sum = (a: number, b: number) => {
