fc7d7713778c4afa623e6f4ccafb0fa596938955
abaez8031 <abaez8031@gmail.com>
2023-06-30 18:11:23 -0400

    postId

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 let myName: string = "Dave";
 let meaningOfLife: number;
 let isLoading: boolean;
-let album: string | number;
+let album: any
 
 myName = "John";
 meaningOfLife = 42;
 isLoading = true;
 album = "Van Halen";
-album = 1984;
+album = 5150;
 
 
 const sum = (a: number, b: number) => {
   return a + b;
 }
+
+let postId: string | number;
