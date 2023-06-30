4f3b2b446978c73441251b5d7b82e76f1a748566
abaez8031 <abaez8031@gmail.com>
2023-06-30 18:15:44 -0400

    lesson 3

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
-let myName: string = "Dave";
-let meaningOfLife: number;
-let isLoading: boolean;
-let album: any
+// let myName: string = "Dave";
+// let meaningOfLife: number;
+// let isLoading: boolean;
+// let album: any
 
-myName = "John";
-meaningOfLife = 42;
-isLoading = true;
-album = "Van Halen";
-album = 5150;
+// myName = "John";
+// meaningOfLife = 42;
+// isLoading = true;
+// album = "Van Halen";
+// album = 5150;
 
 
-const sum = (a: number, b: number) => {
-  return a + b;
-}
+// const sum = (a: number, b: number) => {
+//   return a + b;
+// }
 
-let postId: string | number;
-let isActive: number | boolean;
+// let postId: string | number;
+// let isActive: number | boolean;
+
