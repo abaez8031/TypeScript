100b9c27d837beebe6b0bc1772e27e80c3e0af74
abaez8031 <abaez8031@gmail.com>
2023-07-05 14:46:59 -0400

    optional parameters

--- build/js/main.js
+++ build/js/main.js
@@ -21,3 +21,17 @@ let multiply = function (c, d) {
     return c * d;
 };
 logMsg(multiply(2, 2));
+// optional parameters
+// optional parameter must be last.
+const addAll = (a, b, c) => {
+    if (typeof c !== "undefined") {
+        return a + b + c;
+    }
+    return a + b;
+};
+const sumAll = (a, b, c = 2) => {
+    return a + b + c;
+};
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(2, 3));
--- src/main.ts
+++ src/main.ts
@@ -111,3 +111,19 @@ let multiply: mathFunction = function(c,d) {
 }
 
 logMsg(multiply(2,2))
+
+// optional parameters
+// optional parameter must be last.
+const addAll = (a: number, b: number, c?: number): number => {
+  if (typeof c !== "undefined") {
+    return a + b + c
+  }
+  return a + b
+}
+const sumAll = (a: number, b: number, c: number = 2): number => { 
+    return a + b + c
+}
+
+logMsg(addAll(2,3,2))
+logMsg(addAll(2,3))
+logMsg(sumAll(2,3))
