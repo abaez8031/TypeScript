45cad4fc32343a4ae11f3009e4b24e575dda029a
abaez8031 <abaez8031@gmail.com>
2023-07-05 20:19:53 -0400

    assertions

--- src/main.ts
+++ src/main.ts
@@ -161,3 +161,19 @@
 //   if (typeof value === "string") return "string"
 //   return createError("This should never happen!")
 // }
+
+type One = string;
+type Two = string | number;
+type Three = "hello"
+
+let a: One = "hello";
+let b = a as Two;
+let c = a as Three
+
+let d = <One>"world"
+let e = <string | number>"world"
+
+const addOrConcat = (a: number, b: number, c: "add" | "concat"): number | string => {
+  if(c === "add") return a + b
+  return "" + a + b
+}
