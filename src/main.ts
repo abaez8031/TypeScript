bd585ca6458b4e51cb8ca6a393040ae9dfd22d1f
abaez8031 <abaez8031@gmail.com>
2023-07-11 19:13:54 -0400

    classes

--- src/main.ts
+++ src/main.ts
@@ -183,14 +183,18 @@
 // let nextVal: number = addOrConcat(2,2,"concat") as number
 
 class Coder {
+  secondLang!: string
+
   constructor(
     public readonly name: string, 
     public music: string, 
     private age: number, 
-    protected lang: string) {
+    protected lang: string = "Typescript") {
     this.name = name
     this.music = music
     this.age = age
     this.lang = lang
   }
 }
+
+const Dave = new Coder("Dave", "Rock", 42)
