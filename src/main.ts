1faa85f4946f4eed86c6ace1d1e09f440e959532
abaez8031 <abaez8031@gmail.com>
2023-07-17 18:42:43 -0400

    gettersnsetters

--- src/main.ts
+++ src/main.ts
@@ -265,3 +265,23 @@ const Steve = new Peeps("Steve")
 const Amy = new Peeps("Amy")
 
 console.log(Peeps.count)
+
+class Bands {
+  private dataState: string[];
+
+  constructor() {
+    this.dataState = [];
+  }
+
+  public get data(): string[] {
+    return this.dataState
+  }
+
+  public set data(value: string[]) {
+    if(Array.isArray(value) && value.every(ele => typeof(ele) === "string")) {
+      this.dataState = value;
+      return
+    }
+    else throw new Error("Param is not an array of strings")
+  }
+}
